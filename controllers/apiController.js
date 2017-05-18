a4b5986bdf8171bd3620dee4c089ef2f0c6e870e
ighobverhai aror <ighobverhai aror>
2017-05-18 12:00:07 -0400

    submitting weather API changes

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,6 +1,26 @@
 var express = require('express');
 var apiRouter= express.Router();
+var weather = require("weather-js");
 
+apiRouter.get('/weather/:zipcode', function (req, res) {
+  
+
+weather.find({ search: "30126, GA", degreeType: "C", Date: "Current" }, function(err, result) {
+
+  // If there is an error log it.
+  if (err) {
+    console.log(err);
+  }
+
+  // If there is no error... then print out the weather data.
+  // We use JSON.stringify to print the data in string format.
+  // We use the JSON.stringify argument of "2" to make the format pretty.
+  // See link here: http://stackoverflow.com/questions/4810841/how-can-i-pretty-print-json-using-javascript
+  console.log(JSON.stringify(result, null, 2));
+
+});
+  res.status(200).json(weather);
+});
 
 
 module.exports = apiRouter;
